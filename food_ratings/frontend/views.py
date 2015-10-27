799a8e97639eb7c894909021a6bc1b70963ff9a8
Adam Shimali <adam.shimali@gmail.com>
2015-10-27 17:10:48 +0000

    start of ratings details page

--- food_ratings/frontend/views.py
+++ food_ratings/frontend/views.py
@@ -35,3 +35,9 @@ def search():
     form = SearchForm(business=request.args['business'], location=request.args['location'])
     results = [{'name': 'Byron Hamburgers', 'premises': '1a St Giles High Street, WC2H 8AG', 'inspection_date': '27 August 2015', 'rating': '1 – Major improvement needed'}, {'name': 'Byron Hamburgers', 'premises': '6 Rathbone Place, London, W1T 1HL', 'inspection_date': '1 December 2014', 'rating': '4 – Good'}, {'name': 'Byron Hamburgers', 'premises': '6 Store Street, London, WC1E 7DQ', 'inspection_date': '22 July 2012', 'rating': '5 – Very good'}]
     return render_template('results.html', form=form, results=results)
+
+
+@frontend.route('/rating/<fhrs_id>')
+def rating(fhrs_id):
+    rating = {'name': 'Byron Hamburgers', 'premises': '1a St Giles High Street, WC2H 8AG', 'inspection_date': '27 August 2015'}
+    return render_template('rating.html', rating=rating, fhrs_id=fhrs_id)
